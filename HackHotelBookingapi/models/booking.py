03f2e17e81bc16f39a6c41d78a6256a1febf7892
Kirthana Sukumaran <kirthanaa07@gmail.com>
2024-01-30 19:34:17 -0600

    Ran fixtures

--- HackHotelBookingapi/models/booking.py
+++ HackHotelBookingapi/models/booking.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .room import Room
+from .user import User
 
 
 class Booking(models.Model):
@@ -7,8 +8,8 @@ class Booking(models.Model):
     booking = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    check_in = models.DateTimeField
-    check_out = models.DateTimeField
+    check_in = models.DateField
+    check_out = models.DateField
     no_of_guests = models.IntegerField
     payment_type = models.CharField(max_length=50)
     total_amount = models.IntegerField
